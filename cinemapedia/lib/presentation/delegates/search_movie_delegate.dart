e8ac46eaecb33c212fc8c8d12d0e3b03f1d3523d
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-01-04 16:01:29 -0500

    [Cinemapedia] peticiones http y valores nulos en peliculas

--- cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
+++ cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
@@ -2,7 +2,15 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
+
+typedef SearchMoviesCallBack = Future<List<Movie>> Function (String query);
+
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
+
+  final SearchMoviesCallBack searchMovies;
+
+  SearchMovieDelegate({
+    required this.searchMovies});
 
 
   @override
@@ -16,7 +24,7 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
       FadeIn(
         animate: query.isNotEmpty,
         child: IconButton(
-          onPressed: () => query = "", 
+          onPressed: () => query = "",
           icon: const Icon(Icons.clear)
         ),
       )
@@ -26,7 +34,8 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
-      onPressed: () => close(context, null), icon: const Icon(Icons.arrow_back));
+      onPressed: () => close(context, null), 
+      icon: const Icon(Icons.arrow_back));
   }
 
   @override
@@ -36,7 +45,19 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return const Text("buildSuggestions");
-  }
+    return FutureBuilder(
+      future: searchMovies(query),
+      builder: (context, snapshot) {
 
+        final movies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return ListTile(title: Text(movie.title));
+          },
+        );
+    },) ;
+  }
 }
