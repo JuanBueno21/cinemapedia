aeac84aaed8eddd97190e95f6ef34f73f2bbad3d
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-01-09 11:06:25 -0500

    [Cinemaoedia] search movies Provider
    mantener resultados-valores de la busqueda de peliculas

--- cinemapedia/lib/presentation/providers/providers.dart
+++ cinemapedia/lib/presentation/providers/providers.dart
@@ -4,4 +4,4 @@ export 'movies/movies_providers.dart';
 export 'movies/movies_repository_provider.dart';
 export 'movies/movies_slideshow_provider.dart';
 
-export '';
+export 'search/search_movies_provider.dart';
