8a682f1a2c1138ec766d3b85ed3d53c9001d53df
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-01-16 17:10:55 -0500

    [Cinemapedia] provider y almacenamiento en base de datos
    implementación de los metodos del isar_datasource y almacenamiento y/o inserción en base de datos Isar

--- cinemapedia/lib/infrastructure/datasources/isar_datasource.dart
+++ cinemapedia/lib/infrastructure/datasources/isar_datasource.dart
@@ -3,6 +3,7 @@
 import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasource extends LocalStorageDatasource {
 
@@ -13,29 +14,56 @@ class IsarDatasource extends LocalStorageDatasource {
   }
 
   Future<Isar> opendDB() async {
-    if(Isar.instanceNames.isEmpty) {
-      return await Isar.open([MovieSchema], inspector: true, directory: '');
-    }
 
+    final dir = await getApplicationCacheDirectory();
+
+    if(Isar.instanceNames.isEmpty) {
+      return await Isar.open([MovieSchema], inspector: true, directory: dir.path);
+    }
     return Future.value(Isar.getInstance());
   }
 
   @override
-  Future<bool> isMovieFavorite(int movieId) {
-    // TODO: implement isFavorite
-    throw UnimplementedError();
+  Future<bool> isMovieFavorite(int movieId) async {
+    final isar = await db;
+
+    final Movie? isFavotiteMovie = await isar.movies
+    .filter()
+    .idEqualTo(movieId)
+    .findFirst();
+
+    return isFavotiteMovie != null;
   }
 
   @override
-  Future<void> toggleFavorite(Movie movie) {
-    // TODO: implement toggleFavorite
-    throw UnimplementedError();
+  Future<void> toggleFavorite(Movie movie) async {
+    final isar = await db;
+
+    final favoriteMovie = await isar.movies
+    .filter()
+    .idEqualTo(movie.id)
+    .findFirst();
+
+    if (favoriteMovie != null) {
+      //borrar
+      isar.writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
+      return;
+    }
+
+    // insertar
+    isar.writeTxnSync(() => isar.movies.putSync(movie));
+
   }
 
     @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovie
-    throw UnimplementedError();
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    final isar = await db;
+
+    return isar.movies.where()
+    .offset(offset)
+    .limit(limit)
+    .findAll();
+
   }
 
 }
--- cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -1,8 +1,9 @@
 
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/providers/movies/actors/actors_by_movie_provider.dart';
+import 'package:cinemapedia/presentation/providers/actors/actors_by_movie_provider.dart';
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
+import 'package:cinemapedia/presentation/providers/storage/local_storage_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -52,85 +53,6 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
           )
           )
         ],
-      ),
-    );
-  }
-}
-
-
-
-class _CustomSliverAppBar extends StatelessWidget {
-  
-  final Movie movie;
-
-  const _CustomSliverAppBar({
-    required this.movie});
-
-  @override
-  Widget build(BuildContext context) {
-
-    final size = MediaQuery.of(context).size;
-
-    return SliverAppBar(
-      backgroundColor: Colors.black,
-      expandedHeight: size.height * 0.7,
-      foregroundColor: Colors.white,
-      actions: [
-        IconButton(onPressed: () {
-          
-        }, 
-        
-        icon: const Icon(Icons.favorite_border),
-        // icon: const Icon(Icons.favorite_rounded, color: Colors.red),
-        
-        )
-      ],
-      flexibleSpace: FlexibleSpaceBar(
-        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        
-        //* Ttitulo de la pelicula
-        /*title: Text(
-          movie.title,
-          style: const TextStyle(fontSize: 20),
-          textAlign: TextAlign.start,
-        ), */
-
-        background: Stack(
-          children: [
-            SizedBox.expand(
-              child: Image.network(movie.posterPath,
-              fit: BoxFit.cover,
-              loadingBuilder: (context, child, loadingProgress) {
-                if(loadingProgress != null) return const SizedBox();
-                return FadeIn(child: child);
-              },
-              ),
-            ),
-
-
-
-            const _CustomGradient(
-              begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                stops: [0.0, 0.2],
-                colors: [Colors.black54, Colors.transparent],),
-
-
-            const _CustomGradient(
-              begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                stops: [0.8, 1.0],
-                colors: [Colors.transparent, Colors.black54],),
-
-
-            const _CustomGradient(
-              begin: Alignment.topLeft,
-                
-                stops: [0.0, 0.3],
-                colors: [Colors.black87, Colors.transparent],),
-
-
-        ]),
       ),
     );
   }
@@ -271,6 +193,86 @@ class _ActorByMovie extends ConsumerWidget {
           );
 
         })
+      ),
+    );
+  }
+}
+
+
+class _CustomSliverAppBar extends ConsumerWidget {
+  
+  final Movie movie;
+
+  const _CustomSliverAppBar({
+    required this.movie});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final size = MediaQuery.of(context).size;
+
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      foregroundColor: Colors.white,
+      actions: [
+        IconButton(onPressed: () {
+          
+          ref.watch(localStorageRepositoryProvider).toggleFavorite(movie);
+
+        }, 
+        
+        icon: const Icon(Icons.favorite_border),
+        // icon: const Icon(Icons.favorite_rounded, color: Colors.red),
+        
+        )
+      ],
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        
+        //* Ttitulo de la pelicula
+        /*title: Text(
+          movie.title,
+          style: const TextStyle(fontSize: 20),
+          textAlign: TextAlign.start,
+        ), */
+
+        background: Stack(
+          children: [
+            SizedBox.expand(
+              child: Image.network(movie.posterPath,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, loadingProgress) {
+                if(loadingProgress != null) return const SizedBox();
+                return FadeIn(child: child);
+              },
+              ),
+            ),
+
+
+
+            const _CustomGradient(
+              begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                stops: [0.0, 0.2],
+                colors: [Colors.black54, Colors.transparent],),
+
+
+            const _CustomGradient(
+              begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.8, 1.0],
+                colors: [Colors.transparent, Colors.black54],),
+
+
+            const _CustomGradient(
+              begin: Alignment.topLeft,
+                
+                stops: [0.0, 0.3],
+                colors: [Colors.black87, Colors.transparent],),
+
+
+        ]),
       ),
     );
   }
