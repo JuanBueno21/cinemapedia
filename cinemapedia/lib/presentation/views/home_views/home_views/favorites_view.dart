e6bf828c39555af4b52198766a405ee45be9896c
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-01-17 16:35:36 -0500

    [Cinemapedia] mostrar las peliculas en favoritas

--- cinemapedia/lib/presentation/views/home_views/home_views/favorites_view.dart
+++ cinemapedia/lib/presentation/views/home_views/home_views/favorites_view.dart
@@ -1,17 +1,40 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        title: const Text("Favorites view"),
-      ),
+  FavoritesViewState createState() => FavoritesViewState();
+}
 
-      body: const Center(
-        child: Text("Favoritos"),
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: favoriteMovies.length,
+        itemBuilder: (context, index) { 
+
+          final movie = favoriteMovies[index];
+
+          return ListTile(
+            title: Text(movie.title),
+          );
+         },
       ),
     );
   }
